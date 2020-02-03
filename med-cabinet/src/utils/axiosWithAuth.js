ff1435bc29413cc807e31f4b6d808fb01c68e2d1
George Davila <georgedavila@Georges-MacBook-Pro.local>
2020-02-03 18:32:37 -0500

    day 1 done

--- med-cabinet/src/utils/axiosWithAuth.js
+++ med-cabinet/src/utils/axiosWithAuth.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
 export const axiosWithAuth = () => {
-  //retrieving auth token from localStorage
   const token = localStorage.getItem('token')
-
-  //create axios instance with Auth token
-
+  
   return axios.create({
     baseURL: 'https://localhost:3000',
     headers: {
