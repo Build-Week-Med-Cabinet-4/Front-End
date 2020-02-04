ca567d09f19536c62b439dc6b0cb56fdf9248ff7
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-04 14:41:59 -0600

    added button styling.

--- med-cabinet/src/components/FormStyles/LgSgButtonStyle.jsx
+++ med-cabinet/src/components/FormStyles/LgSgButtonStyle.jsx
@@ -1,5 +1,22 @@
 import styled from "styled-components";
 
 export const LgSgButtonStyle = styled.button`
-  border: 1px solid red;
+  background: white;
+  margin: 15px auto;
+  border: 1px solid #d3d3d3;
+  border-radius: 5px;
+  padding: 5px 15px;
+  font-size: 16px;
+  color: #191919;
+  box-shadow: 0 2px 5px 1px rgba(0, 0, 0, 0.12),
+    0 2px 2px 1px rgba(0, 0, 0, 0.24);
+
+  &:hover {
+    transition: all 0.2 ease-in;
+    transform: translateY(-1px);
+    box-shadow: 0 2px 6px 2px rgba(0, 0, 0, 0.12),
+      0 2px 3px 2px rgba(0, 0, 0, 0.24);
+    background: black;
+    color: white;
+  }
 `;
