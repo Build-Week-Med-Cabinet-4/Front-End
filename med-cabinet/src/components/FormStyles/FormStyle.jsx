53b69aec3e199b4afa4c2a7fdd8659160fa1f78c
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-04 14:08:25 -0600

    changed some styling to the form and label.

--- med-cabinet/src/components/FormStyles/FormStyle.jsx
+++ med-cabinet/src/components/FormStyles/FormStyle.jsx
@@ -4,5 +4,17 @@ export const FormStyle = styled.form`
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
+
+  width: 80vw;
+  height: 50vh;
   margin: 50px auto;
+
+  border-radius: 10px;
+
+  padding: 10px 0;
+  background: #00726a;
+
+  &:hover {
+    transition: all 0.2s ease-in;
+  }
 `;
